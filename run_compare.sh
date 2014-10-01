d32d97872be6d0410cdb658a95237ec943958d56
ZSlie <zsliefert@gmail.com>
2014-10-01 13:37:11 -0500

    Change fasta variable name to passed in variable

--- run_compare.sh
+++ run_compare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+# Usage: run_compare.sh fasta 1 fasta 2
 # qrsh -P KSU-GEN-BIOINFO -l avx=true -pe single 16
-DIR="Fly_project/"
-FASTA="fasta_basename"
-FASTA2="fasta_basename"
+DIR="FlyProject/"
+FASTA="$1"
+FASTA2="$2"
 
 #Align scripts
 /homes/bioinfo/bioinfo_software/bionano/tools/RefAligner -i ${DIR}${FASTA2}.fasta_QI.cmap -ref ${DIR}${FASTA}.fasta_QI.cmap -o ${DIR}${FASTA}_to_${FASTA2} -res 2.9 -FP 0.8 -FN 0.08 -sf 0.20 -sd 0.10 -extend 1 -outlier 1e-4 -endoutlier 1e-2 -deltaX 12 -deltaY 12 -xmapchim 14 -mres 1.2 -insertThreads 4 -nosplit 2 -f -T 1e-8 -maxthreads 16
